0f8792209c01376efc8e070c82d4ec77b0178ca3
vitor-mat <vitormateusalves123@gmail.com>
2021-09-04 02:22:10 -0300

    primeira versão funcional do script de animação da pag feito (pagses estao animadas sempre que entram em tela)

--- script.js
+++ script.js
@@ -1,6 +1,59 @@
-document.querySelector(".container").addEventListener("click", () => {
+const container = document.querySelector(".container")
+
+const pageCount = 0;
+
+function scrollAnimation(){
+
     const pages = document.querySelectorAll("[data-page]");
-    pages[0].classList.add("anima-center")
-    console.log("clicou")
-    console.log(pages[0])
-})
+
+    const viewHeight = container.scrollTop + window.innerHeight
+
+    /*
+    pages.forEach((e) => {
+
+        if(viewHeight > e.offsetTop){
+            e.classList.add("anima-center")
+        }else{
+            e.classList.remove("anima-center")
+        }
+    })
+    */
+
+    console.log(container.scrollTop)
+    console.log(window.innerHeight)
+
+    if(container.scrollTop == 0){
+        pages[0].classList.add("anima-center")
+    }else{
+        pages[0].classList.remove("anima-center")
+    }
+
+    if(container.scrollTop == window.innerHeight){
+        pages[1].classList.add("anima-center")
+    }else{
+        pages[1].classList.remove("anima-center")
+    }
+
+    if(container.scrollTop == 2*window.innerHeight){
+        pages[2].classList.add("anima-center")
+    }else{
+        pages[2].classList.remove("anima-center")
+    }
+
+    if(container.scrollTop == 3*window.innerHeight){
+        pages[3].classList.add("anima-center")
+    }else{
+        pages[3].classList.remove("anima-center")
+    }
+
+    if(container.scrollTop == 4*window.innerHeight){
+        pages[4].classList.add("anima-center")
+    }else{
+        pages[4].classList.remove("anima-center")
+    }
+
+}
+
+scrollAnimation()
+
+container.addEventListener("scroll", scrollAnimation)
